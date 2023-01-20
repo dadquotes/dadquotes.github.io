9e91b7d5f72c5309ed21aa710ce966003bebd850
Rich Williams <rchwms@gmail.com>
2023-01-20 11:59:05 -0600

    first webstorm commit

--- getimages.php
+++ getimages.php
@@ -1,27 +1,44 @@
 <?php
-//PHP SCRIPT: getimages.php
-Header("content-type: application/x-javascript");
- 
-//This function gets the file names of all images in the current directory
-//and ouputs them as a JavaScript array
-function returnimages($dirname=".") {
-    $pattern="(\.jpg$)|(\.png$)|(\.jpeg$)|(\.gif$)"; //valid image extensions
-    $files = array();
-    $curimage=0;
-    if($handle = opendir($dirname)) {
-        while(false !== ($file = readdir($handle))){
-            if(eregi($pattern, $file)){ //if this file is a valid image
-                //Output it as a JavaScript array element
-                echo 'galleryarray['.$curimage.']="'.$file .'";';
-                $curimage++;
+
+echo scanDirectoryImages("images");
+
+/**
+ * Recursively search through directory for images and display them
+ *
+ * @param  array  $exts
+ * @param  string $directory
+ * @return string
+ */
+function scanDirectoryImages($directory, array $exts = array('jpeg', 'jpg', 'gif', 'png'))
+{
+    if (substr($directory, -1) == '/') {
+        $directory = substr($directory, 0, -1);
+    }
+    $html = '';
+    if (
+        is_readable($directory)
+        && (file_exists($directory) || is_dir($directory))
+    ) {
+        $directoryList = opendir($directory);
+        while($file = readdir($directoryList)) {
+            if ($file != '.' && $file != '..') {
+                $path = $directory . '/' . $file;
+                if (is_readable($path)) {
+                    if (is_dir($path)) {
+                        return scanDirectoryImages($path, $exts);
+                    }
+                    if (
+                        is_file($path)
+                        && in_array(end(explode('.', end(explode('/', $path)))), $exts)
+                    ) {
+                        $html .= '<a href="' . $path . '"><img src="' . $path
+                            . '" style="max-height:100px;max-width:100px" /></a>';
+                    }
+                }
             }
         }
- 
-        closedir($handle);
+        closedir($directoryList);
     }
-    return($files);
+    return $html;
 }
- 
-echo 'var galleryarray=new Array();'; //Define array in JavaScript
-returnimages() //Output the array elements containing the image file names
-?>
+
